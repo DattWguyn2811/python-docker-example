c621d45d17efce0ec7e98e7a971fb72176fe3439
Tien Dat Nguyen <dattnguyen602@gmail.com>
2025-06-12 16:26:02 +0700

    a

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # Define a simple route
+## This route will respond with a JSON message when accessed
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
